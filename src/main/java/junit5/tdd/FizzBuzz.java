b07369733c86991826d1a18410dc0eb28fcc4a50
JABIN LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <LIANGJA6@OOCL.COM>
2020-07-23 12:54:00 +0800

    [fizz buzz][Jabin] feature:pass should_return_fizzwhizz_when_count_off_given_21

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,8 +3,11 @@ package junit5.tdd;
 public class FizzBuzz {
     public String play(int inputNumber) {
 
-        if( inputNumber%3 == 0&&inputNumber%5 == 0){
+        if( inputNumber % 3 == 0 && inputNumber % 5 == 0){
             return "FizzBuzz";
+        }
+        if( inputNumber % 3 == 0 && inputNumber % 7 == 0){
+            return "FizzWhizz";
         }
         if( inputNumber%3 == 0){
             return "Fizz";
